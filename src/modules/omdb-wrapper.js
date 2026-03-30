3ccb4b91f9fa73ad42c83b5f4b2040cee97a91e7
FACUNDO ELIEL ANGEL <49375897@ort>
2026-03-30 11:31:45 -0300

    terminado

--- src/modules/omdb-wrapper.js
+++ src/modules/omdb-wrapper.js
@@ -27,42 +27,49 @@ const OMDBSearchByPage = async (searchText, page = 1) => {
 
 
 
-};
-const OMDBSearchComplete = async (searchText) =>
-  {
-  let returnObject = {
-    respuesta: false,
-    cantidadTotal: 0,
-    datos: []
-  };
-
-  let pagina = 1;
-  let resultado = await OMDBSearchByPage(searchText, 1);
-for(let i = 0 ; 1 <= 8 ; i++){
-
-let requestString = `${BASE_URL}&s=${searchText}&page=${page}`
-    const apiResponse = await axios.get(requestString);
-
-
-    if (apiResponse.data.Response === "True") {
-      returnObject.respuesta = true;
-      returnObject.cantidadTotal = (apiResponse.data.totalResults);
-      returnObject.datos = apiResponse.data.Search;
-    }
-   
 }
-  
-    return resultado;
-  };
+const OMDBSearchComplete = async (searchText) => {
+let returnObject = {
+respuesta : false,
+cantidadTotal : 0,
+datos : []};
+let pagina= 1
+for (let i = 0; i <= 8; i++)
+{
+let url  = 'http://www.omdbapi.com/?' + 'apikey=' + APIKEY + '&s=' + searchText  + '&page=' + pagina
+let response = await axios.get(url)
 
-const OMDBGetByImdbID = async (imdbID) => {
-  let returnObject = {
-    respuesta: false,
-    cantidadTotal: 0,
-    datos: {}
-  };
-  // No seas vago, acá hay que hacer el cuerpo de la función!!!
-  return returnObject;
+if(response.data.Response === "True"){
+    returnObject.respuesta = true;
+    returnObject.cantidadTotal = response.data.totalResults;
+ returnObject.datos = returnObject.datos.concat(response.data.Search);
+}
+
+
+pagina++
+}
+
+
+return returnObject;
+
+};const OMDBGetByImdbID = async (imdbID) => {
+let returnObject = {
+respuesta : false,
+cantidadTotal : 0,
+datos : {}
 };
-// Exporto todo lo que yo quiero exponer del módulo:
+
+let url  = 'http://www.omdbapi.com/?' + 'apikey=' + APIKEY + '&' + 'page=1'  + '&' + 'i=' + imdbID
+
+let response = await axios.get(url)
+
+if(response.data.Response === "True"){
+    returnObject.respuesta = true;
+    returnObject.datos = response.data;
+}
+
+
+return returnObject;
+};
+
 export { OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID }
